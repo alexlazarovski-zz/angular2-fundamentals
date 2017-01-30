e492bb7b8b2583a2a14c4ca0efe4506549c1b5a5
Aliaksei Lazarouski <aliaksei_lazarouski@epam.com>
2017-01-30 11:36:08 +0300

    First angular form

---
+++ src/app/simple-form/simple-form.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simple-form',
+  template: `
+    <input #myInput type="text">
+    <button (click)="onClick(myInput)">Click me</button>
+  `,
+  styles: []
+})
+export class SimpleFormComponent implements OnInit {
+
+  onClick(value) {
+      console.log(value);
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
